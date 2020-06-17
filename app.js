d13432960a223eb2aa093e808eaf32c83b9a49df
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-17 18:41:40 -0400

    adding image save to pdf

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const electron = require('electron')
+var electron = require('electron');
 
 electron.app.on('ready', function () {
-  const mainWindow = new electron.BrowserWindow({width: 600, height: 800, webPreferences: {nodeIntegration: true}});
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
-});
+    var mainWindow = new electron.BrowserWindow({
+        width: 600,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+})
--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const picture = require('cat-picture');
-const image = require('lightning-image-poly');
+// Get cat pic src
+var catpicture = require('cat-picture');
+var src = catpicture.src;
+catpicture.remove();
 
-const src = picture.src
-picture.remove()
+// Get annotation
+var image = require('lightning-image-poly');
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
 
-const viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
+// Save pdf
+var fs = require('fs');
+var remote = require('electron').remote;
+
+function save () {
+
+    remote.getCurrentWebContents().printToPDF({portrait: true})
+        .then( data => {
+            fs.writeFile('annotation.pdf', data, function (err) {
+                if (err) {
+                    alert('error generating pdf! ' + err.message);
+                } else {
+                    alert('pdf saved!');
+                }
+            })
+        })
+        .catch (err => {
+            console.log('test');
+        })
+}
+
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode == 80) save()
+})
